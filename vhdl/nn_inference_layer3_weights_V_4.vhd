ec379ba81220a3fe137bb4c1174023a55ee5afe7
Valando <brdza15@gmail.com>
2024-01-07 07:22:17 +0100

    Initial commit

---
+++ vhdl/nn_inference_layer3_weights_V_4.vhd
@@ -0,0 +1,97 @@
+-- ==============================================================
+-- Vitis HLS - High-Level Synthesis from C, C++ and OpenCL v2020.2 (64-bit)
+-- Copyright 1986-2020 Xilinx, Inc. All Rights Reserved.
+-- ==============================================================
+library ieee; 
+use ieee.std_logic_1164.all; 
+use ieee.std_logic_unsigned.all;
+
+entity nn_inference_layer3_weights_V_4_rom is 
+    generic(
+             DWIDTH     : integer := 10; 
+             AWIDTH     : integer := 4; 
+             MEM_SIZE    : integer := 10
+    ); 
+    port (
+          addr0      : in std_logic_vector(AWIDTH-1 downto 0); 
+          ce0       : in std_logic; 
+          q0         : out std_logic_vector(DWIDTH-1 downto 0);
+          clk       : in std_logic
+    ); 
+end entity; 
+
+
+architecture rtl of nn_inference_layer3_weights_V_4_rom is 
+
+signal addr0_tmp : std_logic_vector(AWIDTH-1 downto 0); 
+type mem_array is array (0 to MEM_SIZE-1) of std_logic_vector (DWIDTH-1 downto 0); 
+signal mem : mem_array := (
+    0 => "1100000101", 1 => "1111100011", 2 => "1011111001", 3 => "1111000100", 
+    4 => "0101100111", 5 => "0000000001", 6 => "0010111011", 7 => "1011011100", 
+    8 => "1111001010", 9 => "1110001111" );
+
+
+begin 
+
+
+memory_access_guard_0: process (addr0) 
+begin
+      addr0_tmp <= addr0;
+--synthesis translate_off
+      if (CONV_INTEGER(addr0) > mem_size-1) then
+           addr0_tmp <= (others => '0');
+      else 
+           addr0_tmp <= addr0;
+      end if;
+--synthesis translate_on
+end process;
+
+p_rom_access: process (clk)  
+begin 
+    if (clk'event and clk = '1') then
+        if (ce0 = '1') then 
+            q0 <= mem(CONV_INTEGER(addr0_tmp)); 
+        end if;
+    end if;
+end process;
+
+end rtl;
+
+Library IEEE;
+use IEEE.std_logic_1164.all;
+
+entity nn_inference_layer3_weights_V_4 is
+    generic (
+        DataWidth : INTEGER := 10;
+        AddressRange : INTEGER := 10;
+        AddressWidth : INTEGER := 4);
+    port (
+        reset : IN STD_LOGIC;
+        clk : IN STD_LOGIC;
+        address0 : IN STD_LOGIC_VECTOR(AddressWidth - 1 DOWNTO 0);
+        ce0 : IN STD_LOGIC;
+        q0 : OUT STD_LOGIC_VECTOR(DataWidth - 1 DOWNTO 0));
+end entity;
+
+architecture arch of nn_inference_layer3_weights_V_4 is
+    component nn_inference_layer3_weights_V_4_rom is
+        port (
+            clk : IN STD_LOGIC;
+            addr0 : IN STD_LOGIC_VECTOR;
+            ce0 : IN STD_LOGIC;
+            q0 : OUT STD_LOGIC_VECTOR);
+    end component;
+
+
+
+begin
+    nn_inference_layer3_weights_V_4_rom_U :  component nn_inference_layer3_weights_V_4_rom
+    port map (
+        clk => clk,
+        addr0 => address0,
+        ce0 => ce0,
+        q0 => q0);
+
+end architecture;
+
+
